1e9380186d9a0bb704f1f2d39187115d0bbfd252
Artur <sky.artur.sky@gmail.com>
2020-10-28 10:22:41 +0200

    Add desk and task controllers

---
+++ app/controllers/api/v1/desks_controller.rb
@@ -0,0 +1,54 @@
+class Api::V1::DesksController < ApplicationController
+  before_action :find_desk, only: [:show, :update, :destroy]
+
+  # GET all desks
+  def index
+    @desks = Desk.all
+    render json: @desks
+  end
+
+  # GET /desks/:id
+  def show
+    render json: @user
+  end
+
+  # POST /desks
+  def create
+    @desk = Desk.new(desk_params)
+    if @desk.save
+      render json: @desk
+    else
+      render error: { error: "Unable to create desk" }, status: :bad_request
+    end
+  end
+
+  # PUT /desks/:id
+  def update
+    if @desk
+      @desk.update(user_params)
+      render json: { message: "Desk sucessfully updated" }, status: :ok
+    else
+      render error: { error: "Unable to update Desk" }, status: :bad_request
+    end
+  end
+
+  # DELETE /desks/:id
+  def destroy
+    if @desk
+      @desk.destroy
+      render json: { message: "Desk successfully deleted" }, status: :ok
+    else
+      render error: { error: "Unadle to delete Desk" }, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:desk)
+  end
+
+  def find_desk
+    @desk = Desk.find(params[:id])
+  end
+end
---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,56 @@
+class Api::V1::TasksController < ApplicationController
+    before_action :find_task, only: [:show, :update, :destroy]
+
+  # GET all Tasks
+  def index
+    @task = Task.all
+    render json: @tasks
+  end
+
+  # GET /Tasks/:id
+  def show
+    render json: @user
+  end
+
+  # POST /Tasks
+  def create
+    @task = Task.new(Task_params)
+    if @task.save
+      render json: @task
+    else
+      render error: { error: "Unable to create Task" }, status: :bad_request
+    end
+  end
+
+  # PUT /Tasks/:id
+  def update
+    @task = Task.find(params[:id])
+    if @task
+      @task.update(user_params)
+      render json: { message: "Task sucessfully updated" }, status: :ok
+    else
+      render error: { error: "Unable to update Task" }, status: :bad_request
+    end
+  end
+
+  # DELETE /Tasks/:id
+  def destroy
+    @task = Task.find(params[:id])
+    if @task
+      @task.destroy
+      render json: { message: "Task successfully deleted" }, status: :ok
+    else
+      render error: { error: "Unadle to delete Task" }, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:task)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+end
